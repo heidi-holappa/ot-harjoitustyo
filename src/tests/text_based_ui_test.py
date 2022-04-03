dd28adb97f23fbeba028925203a15726848a3cfe
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-04-03 12:27:43 +0300

    Add user-object to data submission test

--- src/tests/text_based_ui_test.py
+++ src/tests/text_based_ui_test.py
@@ -1,15 +1,18 @@
 import unittest
 from ui.text_based_ui import CounselorSubmit, StartScreen
 from services.console_io import ConsoleIO
+from entities.user import User
 
 class TestTextBasedUI(unittest.TestCase):
     def setUp(self):
         print("Set up goes here")
 
     def test_clear_data(self):
-        data_submit = CounselorSubmit()
+        user = User("testuser", "salasana")
+        data_submit = CounselorSubmit(user)
         for key in data_submit._data:
             data_submit._data[key] = "test content"
         data_submit.clear_data()
         self.assertEqual(0, len(data_submit._data["channel"]))
+
 
