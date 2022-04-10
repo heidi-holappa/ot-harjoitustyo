d3398d8a8cebcedc56a742397ade25021ff1a59d
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-04-10 22:27:14 +0300

    Modify content to work with UserManagement

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -1,34 +1,31 @@
-# This is now in Launch.py --> ask in workshops if that alright.
-# from database_connection import get_database_connection
 from entities.user import User
+from database_connection import get_database_connection
+
 
 class UserRepository:
 
     def __init__(self, connection):
         self._connection = connection
 
-
     def fetch_selected_user(self, username: str):
         cursor = self._connection.cursor()
 
         db_fetch = cursor.execute('''SELECT username, password, role
-                                    FROM USERS WHERE username=?'''\
-                                        ,[username]).fetchone()
+                                    FROM USERS WHERE username=?''', [username]).fetchone()
         fetched_used = User(db_fetch["username"], db_fetch["password"])
         if db_fetch["role"] == 1:
             fetched_used.set_admin()
         return fetched_used
 
-
     def fetch_all_users(self):
         try:
             cursor = self._connection.cursor()
-            
+
             cursor.execute("SELECT username, password, role FROM USERS")
             rows = cursor.fetchall()
-            
+
             users = {}
-            
+
             for row in rows:
                 users[row["username"]] = (row["password"], row["role"])
             return users
@@ -42,10 +39,15 @@ class UserRepository:
             cursor.execute('''INSERT INTO USERS
                         (username, password, role) 
                         VALUES (?,?,?)''',
-                        [new_user.username,
-                        new_user.password,
-                        new_user.role]
-                        )
-            cursor.commit()
+                           [new_user.username,
+                            new_user.password,
+                            new_user.role]
+                           )
+            self._connection.commit()
+            return True
         except Exception as ex:
             print("An error occured. Message: ", ex)
+            return False
+
+
+user_repository = UserRepository(get_database_connection())
