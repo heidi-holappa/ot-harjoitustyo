6f43fbc6781d14014221b0b956fb372257cdba8f
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-04-10 20:31:18 +0300

    add initialize db-connections

--- src/launch.py
+++ src/launch.py
@@ -1,6 +1,15 @@
 from ui import gui
+from repositories.user_repository import UserRepository
+from repositories.contact_data_repository import ContactDataRepository
+from database_connection import get_database_connection
 
 
 
 # text_based_ui.TextBasedUi().start()
+
+# Initialize repositories
+user_repository = UserRepository(get_database_connection())
+contact_repository = ContactDataRepository(get_database_connection())
+# For testing purposes
+#users = user_repository.fetch_all_users()
 gui.main()
