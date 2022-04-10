52722086c117fc0850ca06707046f8fb802a8dff
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-04-10 22:26:24 +0300

    Initial commit. Build SQLite-queries here

--- src/repositories/contact_data_repository.py
+++ src/repositories/contact_data_repository.py
@@ -1,12 +1,13 @@
+from database_connection import get_database_connection
 from entities.contact import Contact
 from entities.user import User
+
 
 class ContactDataRepository:
 
     def __init__(self, connection):
         self._connection = connection
         self._all_data = None
-
 
     def fetch_all_contacts(self):
         self._all_data = {}
@@ -20,22 +21,24 @@ class ContactDataRepository:
             # Perhaps create a contact object and populate it's attributes?
             self._all_data[row["ROWID"]] = row["username"] + ";" + row["datetime"] + ";" + \
                 row["channel"] + ";" + row["type"] + ";" + row["gender"] + ";" + row["age"] + \
-                    ";" + row["content"]
+                ";" + row["content"]
         return self._all_data
-
 
     def add_contact(self, user: User, contact: Contact):
         try:
             cursor = self._connection.cursor()
             cursor.execute('''INSERT INTO CONTACTS
-                    (username, channel, type, age, gender, content) VALUES (?,?,?,?,?)''', \
-                [user.username,
-                contact.channel,
-                contact.type,
-                contact.age,
-                contact.gender,
-                contact.content]
-                )
+                    (username, channel, type, age, gender, content) VALUES (?,?,?,?,?)''',
+                           [user.username,
+                            contact.channel,
+                            contact.type,
+                            contact.age,
+                            contact.gender,
+                            contact.content]
+                           )
             cursor.commit()
         except Exception as ex:
             print("An error occured. Message: ", ex)
+
+
+contact_repository = ContactDataRepository(get_database_connection())
