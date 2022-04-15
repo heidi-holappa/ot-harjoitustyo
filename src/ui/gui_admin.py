c3a6ec32c5f46ee6410a1126ed337e14005949c8
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-04-15 12:12:28 +0300

    refactor, add button to navigate to counselor view

--- src/ui/gui_admin.py
+++ src/ui/gui_admin.py
@@ -7,19 +7,21 @@ class AdminView:
     def __init__(self, 
                 root, 
                 main_view, 
-                counselor_view, 
+                counselor_view,
+                admin_view,
                 user_management=default_user_management):
         
         self._root = root
         self._main_view = main_view
+        self._admin_view = admin_view
         self._counselor_view = counselor_view
                 
         self._frame = None
 
-
-        self._initialize()
         self._contact_management = ContactManagement()
         self._user_management = user_management
+        self._initialize()
+
 
     def pack(self):
         if self._frame:
@@ -40,28 +42,59 @@ class AdminView:
             command=self._main_view
         )
 
-        label.grid(row=0, column=0, pady=5, sticky=constants.W)
-        button_logout.grid(row=0, column=1, padx=10,
-                           pady=5, sticky=constants.E)
-
-        button_logout = ttk.Button(
+        button_counselor = ttk.Button(
             master=self._frame,
             text="Counselor view",
             command=self._counselor_view
         )
 
-        label.grid(row=0, column=1, pady=5, sticky=constants.W)
-        button_logout.grid(row=1, column=1, padx=10,
-                           pady=5, sticky=constants.E)
-
-
-
-        button_submit = ttk.Button(
+        button_admin_stuff = ttk.Button(
             master=self._frame,
             text="Do admin stuff",
             command=self._do_admin_stuff
         )
-        button_submit.grid(row=17, column=1, sticky=constants.E, padx=20)
+
+        label.grid(row=0, column=0, pady=5, sticky=constants.W)
+    
+        button_admin_stuff.grid(row=0, column=1, 
+                            padx=20,
+                            sticky=constants.E)
+        button_counselor.grid(row=0, column=2, 
+                            padx=10, pady=5, 
+                            sticky=constants.E)
+        button_logout.grid(row=0, column=3, 
+                            padx=10, pady=5, 
+                            sticky=constants.E)
+
+
+        self._fetch_contacts()
+
+    def _fetch_contacts(self):
+        contacts = self._contact_management.fetch_all_contacts()
+        labels = {}
+        buttons = {}
+        if contacts:
+            for c_id in contacts: 
+                printout = contacts[c_id]
+                labels[c_id] = ttk.Label(
+                master=self._frame, text=printout)
+                def button_action(x = c_id):
+                    return self._delete_contact(x)
+                buttons[c_id] = ttk.Button(
+                    master=self._frame,
+                    text=f"Delete {c_id}",
+                    command=button_action
+                )
+                labels[c_id].grid(row=c_id+10, column = 1, pady=5, padx=5, sticky=constants.W)
+                buttons[c_id].grid(row=c_id+10, column = 2, pady=5, padx=5, sticky=constants.E)
+            
+        return
+    
+    def _delete_contact(self, c_id):
+        print("Now in delete_contact - method. Contact-id: ", c_id)
+        self._contact_management.delete_contact(c_id)
+        self._admin_view
+
 
     def _do_admin_stuff(self):
         print("Hello admin world!")
