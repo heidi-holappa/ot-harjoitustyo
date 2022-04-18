474fd114446e43284d9ce04cfcb36fd5c55311b9
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-04-18 19:09:40 +0300

    Refactor. Move contact management from GUI-class to service-class

--- src/services/contact_management.py
+++ src/services/contact_management.py
@@ -1,5 +1,6 @@
 from entities.user import User
 from entities.contact import Contact
+from datetime import datetime
 
 from repositories.contact_data_repository import default_contact_repository
 from repositories.user_repository import default_user_repository
@@ -17,13 +18,6 @@ class ContactManagement:
         self._contact_repository = contact_repository
         self._user_management = user_management
 
-    def submit_contact(self, contact: Contact):
-        # print("Now in ContactManagement. Contact content: ",
-        #       contact.content, contact, self._user_management)
-        fetch_user = self._user_management.get_logged_user()
-        # print("user: ", fetch_user, "contact: ", contact)
-        self._contact_repository.add_contact(fetch_user, contact)
-
     def fetch_all_contacts(self):
         return self._contact_repository.fetch_all_contacts()
 
@@ -32,4 +26,19 @@ class ContactManagement:
 
     def delete_contact(self, id):
         self._contact_repository.delete_contact(id)
+
+    def manage_new_contact_submission(self, c_channel, c_type, c_age, c_gender, c_content):
+        time_of_submission = datetime.now()
+        datetime_as_str = time_of_submission.strftime("%d.%m.%Y %H:%M")
+        contact = Contact(datetime_as_str, c_channel, c_type, c_age, c_gender, c_content)
+        result = contact.is_valid()
+        if result[0]:
+            self.submit_contact(contact)
+        return result
+
+    def submit_contact(self, contact: Contact):
+        fetch_user = self._user_management.get_active_user()
+        if not fetch_user:
+            return (False, "Error. No user logged in.")
+        return self._contact_repository.add_contact(fetch_user, contact)
 
