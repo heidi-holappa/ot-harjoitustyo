a45b52af27db5002bcac4f5fb5ebb72b93f28da7
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-04-21 16:51:28 +0300

    Refactor code into methods

--- src/ui/gui_admin.py
+++ src/ui/gui_admin.py
@@ -36,7 +36,16 @@ class AdminView:
         self._frame = Frame(master=self._root, padx=50, pady=50)
         label = ttk.Label(
             master=self._frame, text="You are now in the Admin View")
+        label.grid(row=0, column=0, pady=5, sticky=constants.W)
 
+        self._init_buttons(0)
+        
+        self._init_treeview()
+        self._populate_treeview(self.treeview)
+
+        self.treeview.bind('<<TreeviewSelect>>', self.item_selected)
+
+    def _init_buttons(self, set_row = 0):
         button_logout = ttk.Button(
             master=self._frame,
             text="Logout",
@@ -55,7 +64,6 @@ class AdminView:
             command=self._create_dummy_data
         )
 
-        label.grid(row=0, column=0, pady=5, sticky=constants.W)
 
         button_admin_stuff.grid(row=0, column=0,
                                 padx=20,
@@ -67,112 +75,99 @@ class AdminView:
                            padx=10, pady=5,
                            sticky=constants.E)
 
+    def _init_treeview(self):
         columns = ("username", "date_and_time", "channel",
                    "type", "gender", "age", "content")
-        treeview = ttk.Treeview(
+        self.treeview = ttk.Treeview(
             master=self._frame, columns=columns, show="headings")
-        treeview.heading("username", text="username")
-        treeview.heading("date_and_time", text="date and time")
-        treeview.heading("channel", text="channel")
-        treeview.heading("type", text="type")
-        treeview.heading("gender", text="gender")
-        treeview.heading("age", text="age")
-        treeview.heading("content", text="content")
+        self.treeview.heading("username", text="username")
+        self.treeview.heading("date_and_time", text="date and time")
+        self.treeview.heading("channel", text="channel")
+        self.treeview.heading("type", text="type")
+        self.treeview.heading("gender", text="gender")
+        self.treeview.heading("age", text="age")
+        self.treeview.heading("content", text="content")
 
-        treeview.grid(row=2, column=0, sticky=constants.NSEW)
+        self.treeview.grid(row=2, column=0, sticky=constants.NSEW)
         scrollbar = ttk.Scrollbar(
-            master=self._frame, orient=constants.VERTICAL, command=treeview.yview)
+            master=self._frame, orient=constants.VERTICAL, command=self.treeview.yview)
         scrollbar.grid(row=2, column=1, sticky='ns')
-        # self._fetch_contacts()
-        self._populate_treeview(treeview)
 
-        # textfield = Text(master=self._frame, width=100, height=50, wrap="word")
-        # textfield.grid(row=3, column=0)
-        # textfield["state"] = "disabled"
-
-        def item_selected(event):
-            printout = ""
-            for selected_item in treeview.selection():
-                item = treeview.item(selected_item)
-                parts = item["values"]
-                printout = (f"Username: {str(parts[0])}\n")
-                printout += "date and time: " + str(parts[1]) + "\n"
-                printout += "channel: " + str(parts[2])
-                printout += ", type: " + str(parts[3])
-                n = len(printout) // 100 + 3
-                if parts[4] != "None":
-                    printout += ", gender: " + str(parts[4])
-                    printout += ", age: " + str(parts[5])
-                    printout += "\n\n" + "content:\n"
-                    printout += str(parts[6])
-                    n = len(printout) // 100 + 5
-            textfield = Text(master=self._frame, wrap="word")
-            textfield.grid(row=3, column=0)
-            textfield.insert(1.0, printout)
-            textfield["state"] = "disabled"
-            print(printout)
-
-        treeview.bind('<<TreeviewSelect>>', item_selected)
-
-        # def item_selected(event):
-        #     for selected_item in treeview.selection():
-        #         item = treeview.item(selected_item)
-        #         record = item['values']
-        #         # show a message
-        #         messagebox.showinfo(title='Information', message=','.join(record))
-
-        # treeview.bind('<<TreeviewSelect>>', item_selected)
+    def item_selected(self, event):
+        '''insert selected row into a Text widget'''
+        printout = ""
+        for selected_item in self.treeview.selection():
+            item = self.treeview.item(selected_item)
+            parts = item["values"]
+            printout = (f"Username: {str(parts[0])}\n")
+            printout += "date and time: " + str(parts[1]) + "\n"
+            printout += "channel: " + str(parts[2])
+            printout += ", type: " + str(parts[3])
+            n = len(printout) // 100 + 3
+            if parts[4] != "None":
+                printout += ", gender: " + str(parts[4])
+                printout += ", age: " + str(parts[5])
+                printout += "\n\n" + "content:\n"
+                printout += str(parts[6])
+                n = len(printout) // 100 + 5
+        textfield = Text(master=self._frame, wrap="word")
+        textfield.grid(row=3, column=0)
+        textfield.insert(1.0, printout)
+        textfield["state"] = "disabled"
+        print(printout)
 
     def _populate_treeview(self, treeview):
         contacts = self._contact_management.fetch_all_contacts_as_tuples()
         for contact in contacts:
             treeview.insert('', constants.END, values=contact)
 
-    def _fetch_contacts(self):
-
-        contacts = self._contact_management.fetch_all_contacts()
-        buttons = {}
-        fields = {}
-        if contacts:
-            for c_id in contacts:
-                parts = contacts[c_id].split(";")
-                printout = (f"Username: {str(parts[0])}\n")
-                printout += "date and time: " + str(parts[1]) + "\n"
-                printout += "channel: " + str(parts[2])
-                printout += ", type: " + str(parts[3])
-                n = len(printout) // 100 + 3
-                if parts[4] != "None":
-                    printout += ", gender: " + str(parts[4])
-                    printout += ", age: " + str(parts[5])
-                    printout += "\n\n" + "content:\n"
-                    printout += str(parts[6])
-                    n = len(printout) // 100 + 5
-
-                fields[c_id] = Text(
-                    master=self._frame, width=100, height=n, wrap="word"
-                )
-                fields[c_id].insert(1.0, printout)
-                fields[c_id]["state"] = "disabled"
-                # labels[c_id] = ttk.Label(
-                #     master=self._frame, text=printout, background="white")
-
-                def button_action(x=c_id):
-                    return self._delete_contact(x)
-                buttons[c_id] = ttk.Button(
-                    master=self._frame,
-                    text=f"Delete {c_id}",
-                    command=button_action
-                )
-                fields[c_id].grid(row=c_id+10, column=1,
-                                  pady=5, padx=5, sticky=constants.W)
-                # labels[c_id].grid(row=c_id+10, column=1,
-                #   pady=5, padx=5, sticky=constants.W)
-                buttons[c_id].grid(row=c_id+10, column=2,
-                                   pady=5, padx=5, sticky=constants.E)
-
-        return
-
     def _delete_contact(self, c_id):
         print("Now in delete_contact - method. Contact-id: ", c_id)
         self._contact_management.delete_contact(c_id)
         self._admin_view
+
+
+    # REFACTORED CODE - REMOVE WHEN NOT NEEDED ANYMORE
+    # def _fetch_contacts(self):
+
+        # contacts = self._contact_management.fetch_all_contacts()
+        # buttons = {}
+        # fields = {}
+        # if contacts:
+        #     for c_id in contacts:
+        #         parts = contacts[c_id].split(";")
+        #         printout = (f"Username: {str(parts[0])}\n")
+        #         printout += "date and time: " + str(parts[1]) + "\n"
+        #         printout += "channel: " + str(parts[2])
+        #         printout += ", type: " + str(parts[3])
+        #         n = len(printout) // 100 + 3
+        #         if parts[4] != "None":
+        #             printout += ", gender: " + str(parts[4])
+        #             printout += ", age: " + str(parts[5])
+        #             printout += "\n\n" + "content:\n"
+        #             printout += str(parts[6])
+        #             n = len(printout) // 100 + 5
+
+        #         fields[c_id] = Text(
+        #             master=self._frame, width=100, height=n, wrap="word"
+        #         )
+        #         fields[c_id].insert(1.0, printout)
+        #         fields[c_id]["state"] = "disabled"
+        #         # labels[c_id] = ttk.Label(
+        #         #     master=self._frame, text=printout, background="white")
+
+        #         def button_action(x=c_id):
+        #             return self._delete_contact(x)
+        #         buttons[c_id] = ttk.Button(
+        #             master=self._frame,
+        #             text=f"Delete {c_id}",
+        #             command=button_action
+        #         )
+        #         fields[c_id].grid(row=c_id+10, column=1,
+        #                           pady=5, padx=5, sticky=constants.W)
+        #         # labels[c_id].grid(row=c_id+10, column=1,
+        #         #   pady=5, padx=5, sticky=constants.W)
+        #         buttons[c_id].grid(row=c_id+10, column=2,
+        #                            pady=5, padx=5, sticky=constants.E)
+
+        # return
