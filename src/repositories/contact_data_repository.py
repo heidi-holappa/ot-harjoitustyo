bbd15c6363fd0e7ba761577f525ddc4db3517f8c
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-04-23 13:19:59 +0300

    Add methods/queries for fetching contact information

--- src/repositories/contact_data_repository.py
+++ src/repositories/contact_data_repository.py
@@ -28,7 +28,7 @@ class ContactDataRepository:
         cursor = self._connection.cursor()
 
         cursor.execute('''SELECT ROWID, username, datetime, channel,
-                type, gender, age, content FROM CONTACTS''')
+                type, gender, age, content, marked FROM CONTACTS''')
 
         rows = cursor.fetchall()
         for row in rows:
@@ -40,9 +40,58 @@ class ContactDataRepository:
                 str(row["type"]),
                 str(row["gender"]),
                 str(row["age"]),
-                str(row["content"])
+                str(row["content"]),
+                str(row["marked"])
             ))
         return self._all_data
+
+    def fetch_treeview_contact_info(self):
+        self._all_data = []
+        cursor = self._connection.cursor()
+
+        cursor.execute('''SELECT ROWID, username, datetime, channel,
+                type, marked FROM CONTACTS''')
+
+        rows = cursor.fetchall()
+        for row in rows:
+            self._all_data.append((
+                str(row["ROWID"]),
+                str(row["username"]),
+                str(row["datetime"]),
+                str(row["channel"]),
+                str(row["type"]),
+                str(row["marked"]),
+            ))
+        return self._all_data
+
+    def fetch_selected_contact(self, rowid):
+        cursor = self._connection.cursor()
+
+        db_result = cursor.execute('''SELECT ROWID, 
+                                username, 
+                                datetime, 
+                                channel,
+                                type,
+                                gender,
+                                age,
+                                content,
+                                marked FROM CONTACTS
+                                WHERE ROWID=?
+                                ''', [rowid]).fetchone()
+
+        result = (
+                db_result["ROWID"],
+                db_result["username"],
+                db_result["datetime"],
+                db_result["channel"],
+                db_result["type"],
+                db_result["gender"],
+                db_result["age"],
+                db_result["content"],
+                db_result["marked"],
+            )
+        return result
+
 
     def fetch_contacts_by_user(self, user: User):
         self._all_data = {}
@@ -77,7 +126,8 @@ class ContactDataRepository:
                                 contact.type,
                                 contact.age,
                                 contact.gender,
-                                contact.content]
+                                contact.content,
+                                contact.marked]
                             )
         self._connection.commit()
 
