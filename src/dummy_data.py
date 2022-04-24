4c9eec7654a4073081c84d7ac24845c7ee75d596
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-04-24 09:32:53 +0300

    Separate value of parameter to a variable for testing

--- src/dummy_data.py
+++ src/dummy_data.py
@@ -2,4 +2,6 @@ from services.contact_management import ContactManagement
 
 contact_management = ContactManagement()
 
-contact_management.create_random_contacts(30)
+contact_n = 30
+
+contact_management.create_random_contacts(contact_n)
