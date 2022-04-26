ecac198c15fc32c154410d065eb8f65bfa4d0d83
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-04-26 18:40:50 +0300

    Fix issues based on pylint feedback

--- src/dummy_data.py
+++ src/dummy_data.py
@@ -2,6 +2,6 @@ from services.contact_management import ContactManagement
 
 contact_management = ContactManagement()
 
-contact_n = 30
+CONTACT_N = 30
 
-contact_management.create_random_contacts(contact_n)
+contact_management.create_random_contacts(CONTACT_N)
