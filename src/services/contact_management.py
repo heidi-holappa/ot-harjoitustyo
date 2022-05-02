63c34e6292a69af704a8e585a9d04417451d02b9
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-05-02 17:38:18 +0300

    Add doctstring

--- src/services/contact_management.py
+++ src/services/contact_management.py
@@ -10,6 +10,13 @@ from services.user_management import default_user_management
 
 
 class ContactManagement:
+    """A service class for managing contact-related functionalities
+
+    Attributes:
+        user_repository: a default repository for database queries
+        contact_repository: a default repository for database queries
+        user_management: default service class for user management
+    """
 
     def __init__(self,
                  user_repository=default_user_repository,
@@ -22,27 +29,73 @@ class ContactManagement:
 
     # DELETE THIS WHEN NOT NEEDED ANY MORE
     def fetch_all_contacts(self):
+        """A method to be deleted when made sure it is not needed.
+
+        Returns:
+            dict: returns a dictionary with all stored contacts
+        """
         return self._contact_repository.fetch_all_contacts()
 
     def fetch_all_contacts_as_tuples(self):
+        """Returns contact data as tuples for the treeview widget
+
+        Returns:
+            list: a list populated with tuples
+        """
         return self._contact_repository.fetch_all_contacts_as_tuples()
 
     def fetch_selected_contact(self, rowid):
+        """Returns the selected contact as a tuple
+
+        Args:
+            rowid (int): id of requested contact
+
+        Returns:
+            tuple: tuple of contact information
+        """
         return self._contact_repository.fetch_selected_contact(rowid)
 
     def fetch_treeview_contact_info(self):
+        """A method to query more compact data to populate the treeview widget
+
+        Returns:
+            list: list of tuples for the treeview
+        """
         return self._contact_repository.fetch_treeview_contact_info()
 
     def fetch_contacts_by_user(self, user: User):
+        """Queries contacts from a selected user. 
+
+        This method is not used. Will be deleted.
+
+        Args:
+            user (User): selected user
+
+        Returns:
+            dict: a dictionary of contacts
+        """
         return self._contact_repository.fetch_contacts_by_user(user)
 
     def delete_contact(self, c_id):
+        """A method to query a contact deletion
+
+        Args:
+            c_id (_type_): An id to identify the contact to be deleted
+        """
         self._contact_repository.delete_contact(c_id)
 
     def delete_all_contacts(self):
+        """A method to query the deletion of all contact data
+        """
         self._contact_repository.delete_all_data()
 
     def mark_contact_for_deletion(self, c_id, status):
+        """Alters the deletion status of a contact.
+
+        Args:
+            c_id (int): an id to identify the selected contact
+            status (str): given new status
+        """
         if status == "TRUE":
             mark = "TRUE"
         else:
@@ -50,14 +103,36 @@ class ContactManagement:
         self._contact_repository.mark_for_deletion(c_id, mark)
 
     def _get_current_time_as_str(self):
+        """A method for creating a string of the current date and time
+
+        Returns:
+            String: A string of date and time
+        """
         time_of_submission = datetime.now()
         datetime_as_str = time_of_submission.strftime("%d.%m.%Y %H:%M")
         return datetime_as_str
 
     def delete_marked_contacts(self):
+        """A method to query the deletion of of all contacts marked for deletion.
+        """
         self._contact_repository.delete_marked()
 
     def manage_new_contact_submission(self, c_channel, c_type, c_age, c_gender, c_content):
+        """A method to manage submission of a new contact. 
+
+        Calls methods to create a date and time String and to validate given data.
+
+        Args:
+            c_channel (str): selected channel
+            c_type (str): selected type
+            c_age (str): selected age
+            c_gender (str): selected gender
+            c_content (str): written content
+
+        Returns:
+            (boolean, String): returns a boolean indicating whether submission succeeded 
+            and a string of possible status information
+        """
         datetime_as_str = self._get_current_time_as_str()
         if c_type != 1:
             c_age = 0
@@ -71,6 +146,15 @@ class ContactManagement:
         return result
 
     def submit_contact(self, contact: Contact):
+        """A method for submitting a contact
+
+        Args:
+            contact (Contact): New contact to be submitted
+
+        Returns:
+            (True, String): If succeeds, returns True. 
+            (False, String): If fails, returns False and a string of status information.
+        """
         fetch_user = self._user_management.get_active_user()
         if not fetch_user:
             return (False, "Error. No user logged in.")
@@ -78,6 +162,8 @@ class ContactManagement:
         return self._contact_repository.add_contact(username, contact)
 
     def create_random_contact(self):
+        """A method for creating dummy data
+        """
         rand_users = ["carol", "cynthia", "max", "alex", "murphy", "peter",
                       "jill", "jane", "rhonda", "whoopie", "keanu", "johnny", "fiona"]
         random_user = rand_users[randint(0, len(rand_users)-1)]
@@ -100,6 +186,20 @@ class ContactManagement:
                                         c_age: int,
                                         c_gender: int,
                                         c_content: str):
+        """A method for managing dummy data creation
+
+        Args:
+            user (str): dummy username
+            c_channel (int): dummy channel
+            c_type (int): dummy type
+            c_age (int): dummy age
+            c_gender (int): dummy gender
+            c_content (str): dummy content
+
+        Returns:
+            (True, String): If success, return True and an empty string
+            (False, String): False if something fails and a String of status information
+        """
         datetime_as_str = self._get_current_time_as_str()
         if c_type != 1:
             c_age = 0
@@ -113,13 +213,34 @@ class ContactManagement:
         return result
 
     def _submit_dummy_contact(self, user: str, contact: Contact):
+        """A method for submitting new dummy contact
+
+        Args:
+            user (str): a username
+            contact (Contact): contact object
+        """
         self._contact_repository.add_contact(user, contact)
 
     def create_random_contacts(self, given_n=10):
+        """A method for creating multiple dummy contacts.
+
+        Args:
+            given_n (int, optional): Number of contacts to create. Defaults to 10.
+        """
         for _ in range(given_n):
             self.create_random_contact()
 
     def create_dummy_content(self, contact: Contact):
+        """A method for creating dummy content from dummy quantitative data and lorem ipsum.
+
+        In selected topics referral information is also created. 
+
+        Args:
+            contact (Contact): contact object
+
+        Returns:
+            String: returns created content.
+        """
         c_channel = contact.channel
         age = contact.age
         randtopic = [
--- src/services/user_management.py
+++ src/services/user_management.py
@@ -3,29 +3,81 @@ from entities.user import User
 
 
 class UserManagement:
+    """A class for managing user related functionalities
+
+    Attributes:
+        user_repository: default repository for database queries
+        _active_user: current logged in user (if any)
+    """
     def __init__(self,
                  user_repository=default_user_repository):
+        """A constructor for the class
+
+        Args:
+            user_repository (_type_, optional): Defaults to default_user_repository.
+        """
         self._user_repository = user_repository
         self._active_user = None
 
     def get_user(self, username: str):
+        """A method to query fetching of selected user
+
+        Args:
+            username (str): username to query for
+
+        Returns:
+            None: if no user is found
+            User: user object of information found with the username
+        """
         return self._user_repository.fetch_selected_user(username)
 
     def get_active_user_role(self):
+        """A method to retrieve the role of the logged in user
+
+        Returns:
+            String: returns the value of the enum from class Role
+        """
         if self._active_user:
             return self._active_user.get_role()
         return None
 
+    # DELETE IF NOT USED
     def get_all_users(self):
+        """A method for querying all user data. 
+
+        Might not be used anymore. Make sure and delete if not in use. 
+
+        Returns:
+            dict: Returns a dictionary of all users
+        """
         return self._user_repository.fetch_all_users()
 
     def add_user(self, user: User):
+        """Calls a repository method to add a new user
+
+        Args:
+            user (User): User object to be added
+
+        Returns:
+            True: if all succeeds
+            False: if something fails
+        """
         result = self._user_repository.add_user(user)
         if result:
             return True
         return False
 
     def login(self, username, password):
+        """A method for attempting to login with the information provided
+
+        Args:
+            username (str): username submitted
+            password (str): password submitted
+
+        Returns:
+            (False, None): If validation fails, return False and no role
+            (True, String): if succeeds, return True and the role for the user
+        """
         user = User(username, password)
         self.set_active_user(user)
         user_found = self.get_user(user.username)
@@ -37,9 +89,20 @@ class UserManagement:
         return (False, None)
 
     def get_active_user(self):
+        """Gets the attribute self._active_user
+
+        Returns:
+            None: If no user is currently logged in
+            User: If a user is logged in.
+        """
         return self._active_user
 
     def set_active_user(self, user: User):
+        """Sets a user object as the active user
+
+        Args:
+            user (User): a user object of current logged in user
+        """
         self._active_user = user
 
     def handle_user_creation(self,
@@ -47,6 +110,20 @@ class UserManagement:
                              password1: str,
                              password2: str,
                              is_admin: int):
+        """A method for handling account creation.
+
+        Validates given information and calls for user creation if data is valid. 
+
+        Args:
+            username (str): given username
+            password1 (str): given password
+            password2 (str): given re-typed password
+            is_admin (int): boolean to indicate whether an admin account is to be created
+
+        Returns:
+            (False, String): If validation fails, returns False and a status as a String.
+            method call: calls method self.create_user() which return a tuple
+        """
         new_user = User(username, password1)
         valid_username = new_user.username_is_valid()
         if not valid_username[0]:
@@ -60,6 +137,14 @@ class UserManagement:
         return self.create_user()
 
     def create_user(self):
+        """A method for creating a new user.
+
+        Handles repository queries, checks that username does not exist.
+
+        Returns:
+            (True, String): If success, return True
+            (False, String): If fails, return False and a status notification as a String. 
+        """
         if not self._active_user:
             return (False, "No active user created.")
         username_found = self.get_user(self._active_user.username)
@@ -70,10 +155,14 @@ class UserManagement:
         return (True, "")
 
     def make_admin(self):
+        """Set current user role to Role.ADMIN
+        """
         if self._active_user:
             self._active_user.set_admin()
 
     def logout(self):
+        """Clear active user.
+        """
         self._active_user = None
 
 
