68517e4f9500ceb54386c1dd65eb8669f031dbef
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-05-11 16:14:13 +0300

    Refactor code, add status printout to console

--- src/dummy_data.py
+++ src/dummy_data.py
@@ -4,4 +4,9 @@ contact_management = ContactManagement()
 
 CONTACT_N = 30
 
-contact_management.create_random_contacts(CONTACT_N)
+result, status_msg = contact_management.create_random_contacts(CONTACT_N)
+
+if result:
+    print("Dummy data created successfully.")
+else:
+    print("Data creation failed. Status message: ", status_msg)
