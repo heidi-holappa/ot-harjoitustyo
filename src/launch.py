ebfa708d8379eb631ba0bf8123a6286b9c37b464
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-05-13 19:07:17 +0300

    Remove non-functioning database-test

--- src/launch.py
+++ src/launch.py
@@ -1,15 +1,3 @@
-from gpg import Data
 from ui import gui
-from repositories.user_repository import default_user_repository
-
-class DatabaseInitalizationError(Exception):
-    pass
-
-try:
-    default_user_repository.fetch_all_users()
-except(DatabaseInitalizationError):
-    print("""There was an error with the database.
-            Did you remember to run 'Poetry run invoke build'
-            before launching the application?""")
 
 gui.main()
