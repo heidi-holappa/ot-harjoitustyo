6a3a312090a736c81dc3277472c810ae360bc888
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-05-15 18:22:38 +0300

    Create tests for missing features

--- src/tests/contact_test.py
+++ src/tests/contact_test.py
@@ -64,10 +64,79 @@ class TestContactManagement(unittest.TestCase):
 
     def test_validity_check_fails_when_counseling_contact_is_missing_type(self):
         result = self.contact_management.manage_new_contact_submission(
-            0, 1, 1, 1, "")
+            0, 1, 1, 1, "lorem ipsum")
         self.assertEqual(result[0], False)
 
     def test_validity_check_passes_when_counseling_has_age_gender_content(self):
         result = self.contact_management.manage_new_contact_submission(
             1, 1, 1, 1, "lorem ipsum")
         self.assertEqual(result[0], True)
+
+    def test_contact_is_returned_as_a_tuple(self):
+        self.contact_management.manage_new_contact_submission(
+                1, 1, 1, 1, "lorem ipsum")
+        all_contacts = self.contact_management.fetch_all_contacts_as_tuples()
+        row_id = all_contacts[0][0]
+        contact = self.contact_management.fetch_selected_contact(row_id)
+        self.assertEqual(tuple, type(contact))
+
+    def test_returned_contact_is_correct(self):
+        self.contact_management.manage_new_contact_submission(
+                1, 1, 1, 1, "lorem ipsum 1")
+        all_contacts = self.contact_management.fetch_all_contacts_as_tuples()
+        row_id = all_contacts[0][0]
+        self.contact_management.manage_new_contact_submission(
+                1, 1, 1, 1, "lorem ipsum 2")
+        contact = self.contact_management.fetch_selected_contact(row_id)
+        content = contact[7]
+        self.assertEqual("lorem ipsum 1", content)
+    
+    def test_treeview_content_value_count_matches(self):
+        self.contact_management.create_random_contacts(10)
+        treeview_values = self.contact_management.fetch_treeview_contact_info()
+        n_of_values = len(treeview_values[0])
+        self.assertEqual(6, n_of_values)
+    
+    def test_contact_deletion_succeeds(self):
+        n = 10
+        self.contact_management.create_random_contacts(n)
+        self.contact_management.delete_contact(1)
+        n_after_deletion = len(self.contact_management.fetch_all_contacts_as_tuples())
+        self.assertEqual(n-1, n_after_deletion)
+
+    def test_deleting_all_data_is_successful(self):
+        n = 10
+        self.contact_management.create_random_contacts(n)
+        n_before_deletion = len(self.contact_management.fetch_all_contacts_as_tuples())
+        self.contact_management.delete_all_contacts()
+        n_after_deletion = len(self.contact_management.fetch_all_contacts_as_tuples())
+        self.assertEqual(n_before_deletion - n, n_after_deletion)
+
+    def test_marking_contact_for_deletion_works(self):
+        n = 10
+        self.contact_management.create_random_contacts(n)
+        self.contact_management.mark_contact_for_deletion(1, "TRUE")
+        self.contact_management.delete_marked_contacts()
+        n_after_deletion = len(self.contact_management.fetch_all_contacts_as_tuples())
+        self.assertEqual(n-1, n_after_deletion)
+
+    def test_remove_marking_from_contact_marked_for_deletion_works(self):
+        n = 10
+        self.contact_management.create_random_contacts(n)
+        self.contact_management.mark_contact_for_deletion(1, "TRUE")
+        self.contact_management.mark_contact_for_deletion(1, "")
+        self.contact_management.delete_marked_contacts()
+        n_after_deletion = len(self.contact_management.fetch_all_contacts_as_tuples())
+        self.assertEqual(n, n_after_deletion)
+
+    def test_gender_removed_from_non_counseling_contact(self):
+        self.contact_management.manage_new_contact_submission(
+                1, 2, 1, 1, "lorem ipsum 2")
+        contact = self.contact_management.fetch_all_contacts_as_tuples()
+        self.assertEqual("None", contact[0][5])
+    
+    def test_age_removed_from_non_counseling_contact(self):
+        self.contact_management.manage_new_contact_submission(
+                1, 2, 1, 1, "lorem ipsum 2")
+        contact = self.contact_management.fetch_all_contacts_as_tuples()
+        self.assertEqual("None", contact[0][6])
--- src/tests/user_test.py
+++ src/tests/user_test.py
@@ -19,21 +19,60 @@ class TestUserManagement(unittest.TestCase):
         self.assertEqual(True, result[0])
 
     def test_set_admin(self):
+        user = User("testuser", "password")
+        self.user_management.set_active_user(user)
         self.user_management.make_admin()
         role = self.user_management.get_active_user_role()
         self.assertEqual(Role.ADMIN, role)
 
+    def test_username_is_valid(self):
+        self.dummy_user = User("testuser", "password")
+        is_valid, status_msg = self.dummy_user.username_is_valid()
+        self.assertEqual(True, is_valid)
+
+    def test_username_is_not_valid(self):
+        self.dummy_user = User("abc", "password")
+        is_valid, status_msg = self.dummy_user.username_is_valid()
+        self.assertEqual(False, is_valid)
+
+    def test_handle_user_creation_returns_false_when_username_not_valid(self):
+        is_valid, status_msg = self.user_management.handle_user_creation("abc", "password", "password", False)
+        self.assertEqual(False, is_valid)
+
+    def test_handle_user_creation_returns_false_when_password_not_valid(self):
+        is_valid, status_msg = self.user_management.handle_user_creation("username", "pass", "pass", False)
+        self.assertEqual(False, is_valid)
+        
     def test_valid_password_false(self):
         password_1 = "password"
         password_2 = "passwoerd"
-        result = self.dummy_user.password_is_valid(password_1, password_2)
-        self.assertEqual(False, result[0])
+        result, status_msg = self.dummy_user.password_is_valid(password_1, password_2)
+        self.assertEqual(False, result)
+
+    def test_passwords_do_not_match_error_msg(self):
+        password_1 = "password"
+        password_2 = "passwoerd"
+        result, status_msg = self.dummy_user.password_is_valid(password_1, password_2)
+        self.assertEqual("Error: Passwords do not match. ", status_msg)
+
+    def test_password_not_long_enough_error_msg(self):
+        password_1 = "pass"
+        password_2 = "pass"
+        result, status_msg = self.dummy_user.password_is_valid(password_1, password_2)
+        self.assertEqual("Error: Password must have 6 characters", status_msg)
 
     def test_valid_password_true(self):
         password_1 = "password"
         password_2 = "password"
-        result = self.dummy_user.password_is_valid(password_1, password_2)
-        self.assertEqual(True, result[0])
+        result, status_msg = self.dummy_user.password_is_valid(password_1, password_2)
+        self.assertEqual(True, result)
+
+    def test_login_fails_with_incorrect_credentials(self):
+        user = User("testuser", "password")
+        self.user_management.set_active_user(user)
+        self.user_management.create_user()
+        result, status_msg = self.user_management.login("testuser", "passcode")
+        self.assertEqual(False, result)
 
     def test_get_user(self):
         username = "testuser2"
@@ -68,6 +107,26 @@ class TestUserManagement(unittest.TestCase):
         is_admin = False
         self.user_management.handle_user_creation(
             username, password1, password2, is_admin)
-        result = self.user_management.handle_user_creation(
+        result, status_msg = self.user_management.handle_user_creation(
             username, password1, password2, is_admin)
-        self.assertEqual(False, result[0])
+        self.assertEqual(False, result)
+    
+    def test_when_no_active_user_return_none_for_get_active_user(self):
+        self.user_management._active_user = None
+        result = self.user_management.get_active_user()
+        self.assertEqual(None, result)
+
+    def test_when_no_active_user_return_none_for_active_user_role(self):
+        self.user_management._active_user = None
+        result = self.user_management.get_active_user_role()
+        self.assertEqual(None, result)
+
+    def test_logout_clears_active_user(self):
+        self.user_management.logout()
+        user = self.user_management.get_active_user()
+        self.assertEqual(None, user)
+
+    def test_create_user_method_returns_false_if_no_active_user(self):
+        self.user_management._active_user = None
+        result, status_msg = self.user_management.create_user()
+        self.assertEqual(False, result)
