ee9d47329319ddf1499018c5209ad78b4a5a12d6
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-05-15 18:24:18 +0300

    Add user friendly printout

--- src/dummy_data.py
+++ src/dummy_data.py
@@ -1,8 +1,14 @@
+"""Used to create dummy data from command line. User can configure the number
+of contacts to be created. 
+
+The condition check informs of failure if CONTACT_N < 1 or CONTACT_N > 100. 
+"""
+
 from services.contact_management import ContactManagement
 
 contact_management = ContactManagement()
 
-CONTACT_N = 30
+CONTACT_N = 10
 
 result, status_msg = contact_management.create_random_contacts(CONTACT_N)
 
